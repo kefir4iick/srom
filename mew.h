c5afc1d62c0b86535dbfaf773b90beb6224e6e63
kefir4iick <lizardm17@icloud.com>
2025-12-20 10:30:11 +0200

    update h

--- mew.h
+++ mew.h
@@ -8,6 +8,7 @@
 
 typedef struct {
     uint32_t numberArray[NUM_LEN];
+    bool negative;
     bool chozabretto;
 } Mew;
 
@@ -16,8 +17,8 @@ Mew      zero(void);
 Mew      newm(void);
 Mew      from_u32(uint32_t n);
 Mew      from_hex(const char *hex);
-char*    to_hex(const Mew *a);
 
+char*    to_hex(const Mew *a);
 
 Mew      copy(const Mew *a);
 bool     is_zero(const Mew *a);
@@ -28,33 +29,34 @@ bool     is_even(const Mew *a);
 int      cmp(const Mew *a, const Mew *b);
 void     print_hex(const Mew *a);
 
-
 Mew shift_left(const Mew *a, int bits);
 Mew shift_right(const Mew *a, int bits);
-Mew shift_digits_high(const Mew *a, int shift);
-Mew shift_digits_low(const Mew *a, int shift);
-
+Mew shift_digits_high(const Mew *a, int shift_words);
+Mew shift_digits_low(const Mew *a, int shift_words);
 
 Mew add(const Mew *a, const Mew *b);
 Mew sub(const Mew *a, const Mew *b);
 Mew mul_one(const Mew *a, uint32_t b);
 Mew mul(const Mew *a, const Mew *b);
 Mew sqr(const Mew *a);
+
 Mew divm(const Mew *num, const Mew *den);
+Mew modm(const Mew *num, const Mew *den);
+
 Mew powm(const Mew *base, const Mew *exp);
 
-Mew modm(const Mew *num, const Mew *den);
 Mew gcd(const Mew *a, const Mew *b);
 Mew lcm(const Mew *a, const Mew *b);
 
-Mew add_mod(const Mew *a, const Mew *b, const Mew *m);
-Mew sub_mod(const Mew *a, const Mew *b, const Mew *m);
-Mew mul_mod(const Mew *a, const Mew *b, const Mew *m);
-Mew sqr_mod(const Mew *a, const Mew *m);
-Mew pow_mod(const Mew *base, const Mew *exp, const Mew *mod);
+Mew modm(const Mew *a, const Mew *mod);
+Mew mod_add(const Mew *a, const Mew *b, const Mew *mod);
+Mew mod_subtract(const Mew *a, const Mew *b, const Mew *mod);
 
-Mew compute_barrett_mu(const Mew *m);
-Mew barrett_reduce(const Mew *x, const Mew *m, const Mew *mu);
+Mew barrett_mu(const Mew *mod);
+Mew barrett_reduction(const Mew *x, const Mew *mod, const Mew *mu);
 
+Mew mod_multiply(const Mew *a, const Mew *b, const Mew *mod);
+Mew mod_square(const Mew *a, const Mew *mod);
+Mew mod_pow_barrett(const Mew *base, const Mew *exp, const Mew *mod);
 
-#endif 
+#endif
